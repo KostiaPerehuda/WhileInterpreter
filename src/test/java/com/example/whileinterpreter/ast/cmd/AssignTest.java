9745dcacd972ca6893569613d6564913b9c64f85
Kostiantyn Perehuda <kostiantyn.perehuda@student.manchester.ac.uk>
2021-07-16 10:02:10 +0300

    Add mockito library to project

---
+++ src/test/java/com/example/whileinterpreter/ast/cmd/AssignTest.java
@@ -0,0 +1,25 @@
+package com.example.whileinterpreter.ast.cmd;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.example.whileinterpreter.state.State;
+
+public class AssignTest {
+	
+	private State state;
+
+	@Before
+	public void setUp() throws Exception {
+		state = mock(State.class);
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
