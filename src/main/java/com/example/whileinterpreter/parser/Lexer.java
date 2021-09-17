ba4d7364e8861758ee56497c204b6abfbc23a224
Kostiantyn Perehuda <kostiantyn.perehuda@student.manchester.ac.uk>
2021-09-17 19:30:07 +0300

    Refactor lexical analyzer for better readability
    The refactoring decreased the amount of code in the TokenType enum
    and preserved all the functional properties of the lexical analyzer.
    Also updated Lexer.java to parse the text line by line so that it is
    possible to get a line number of currently processed token.

--- src/main/java/com/example/whileinterpreter/parser/Lexer.java
+++ src/main/java/com/example/whileinterpreter/parser/Lexer.java
@@ -1,39 +1,48 @@
 package com.example.whileinterpreter.parser;
 
 import java.util.LinkedList;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Lexer {
 
+	private static Pattern pattern = Pattern.compile(TokenType.fullPattern);
+
 	public static LinkedList<Token> lex(String input) {
+		return lex(input.split("\n"));
+	}
+
+	public static LinkedList<Token> lex(String... input) {
 		LinkedList<Token> tokens = new LinkedList<Token>();
 
-		Pattern p = Pattern.compile(TokenType.fullPattern);
-		Matcher m = p.matcher(input);
+		Matcher m;
 		String group;
-		TokenType t;
 
-		while (true) {
-			if (!m.find()) {
-				if (m.hitEnd()) break;
-				else throw new RuntimeException("Unexpected Symbol!");
+		for (int row = 0; row < input.length; row++) {
+			m = pattern.matcher(input[row]);
+
+			while (true) {
+				if (!m.find()) {
+					if (m.hitEnd()) break;
+					else throw new RuntimeException("Unexpected Symbol!");
+				}
+
+				for (TokenType t : TokenType.values) {
+
+					group = m.group();
+
+					if (group.matches(t.getPattern())) {
+						if (t == TokenType.INVALID_TOKEN)
+							throw new RuntimeException("Lexical Error at " + group + "!");
+
+						if (t != TokenType.WHITESPACE)
+							tokens.add(new Token(t, group));
+
+						break;
+					}
+				}
 			}
-
-			for (int groupIdx = 1; groupIdx <= TokenType.values.length; groupIdx++) {
-				if ((group = m.group(groupIdx)) == null) continue;
-
-				t = TokenType.fromInt(groupIdx);
-
-				if (t == TokenType.INVALID_TOKEN)
-					throw new RuntimeException("Lexical Error at " + group + "!");
-
-				if (t != TokenType.WHITESPACE)
-					tokens.add(new Token(t, group));
-
-				break;
-			}
-
 		}
 		return tokens;
 	}
--- src/main/java/com/example/whileinterpreter/parser/TokenType.java
+++ src/main/java/com/example/whileinterpreter/parser/TokenType.java
@@ -6,25 +6,17 @@ import java.util.stream.Stream;
 public enum TokenType {
 
 	CONTROL_SYMBOL("\\{", "\\}", "<=", "==", "\\*", "&&", "[!=;()+-]"),
-	KEYWORD("skip", "if", "else", "while", "true", "false"),
-	IDENTIFIER("[a-zA-Z_][a-zA-Z_0-9]*"),
-	NUMBER("\\d+"),
+	KEYWORD("\\bskip\\b", "\\bif\\b", "\\belse\\b", "\\bwhile\\b", "\\btrue\\b", "\\bfalse\\b"),
+	IDENTIFIER("\\b[a-zA-Z_][a-zA-Z_0-9]*\\b"),
+	NUMBER("\\b\\d+\\b"),
 	WHITESPACE("\\p{javaWhitespace}+"),
 
-	INVALID_TOKEN(null);
+	INVALID_TOKEN(".+(?=" + computePattern(CONTROL_SYMBOL, WHITESPACE) + "|$)");
 
-	static {
-		CONTROL_SYMBOL.makeDelimiter();
-		WHITESPACE.makeDelimiter();
-
-		INVALID_TOKEN.pattern = ".+(?=" + computeDelimiterPattern() + "|$)";
-	}
-
-	public static final String fullPattern = computeFullPattern();
-	public static final TokenType[] values = TokenType.values();
+	public static final TokenType[] values = values();
+	public static final String fullPattern = computePattern(values);
 
 	private String pattern;
-	private boolean delimiter;
 
 	private TokenType(String pattern, String... patterns) {
 		this.pattern = pattern;
@@ -33,43 +25,12 @@ public enum TokenType {
 		}
 	}
 
-	private void makeDelimiter() {
-		delimiter = true;
-	}
-
-	private boolean isDelimiter() {
-		return delimiter;
-	}
-
 	public String getPattern() {
 		return pattern;
 	}
 
-	public static TokenType fromInt(int ordinal) {
-		try {
-			return values[ordinal-1];
-		} catch (IndexOutOfBoundsException e) {
-			return null;
-		}
-	}
-
-	private static String computeFullPattern() {
-		String delimiter = computeDelimiterPattern();
-		String wrappedPatternTemplate = String.format("(?<=^|%s)%s(?=%s|$)", delimiter, "%s", delimiter);
-
-		return Stream.of(TokenType.values())
-				.map(t -> {
-					if (t.isDelimiter() || t == TokenType.INVALID_TOKEN) {
-						return "(" + t.getPattern() + ")";
-					}
-					return "(" + String.format(wrappedPatternTemplate, t.getPattern()) + ")";
-				})
-				.collect(Collectors.joining("|"));
-	}
-
-	private static String computeDelimiterPattern() {
-		return Stream.of(TokenType.values())
-				.filter(TokenType::isDelimiter)
+	private static String computePattern(TokenType... tokens) {
+		return Stream.of(tokens)
 				.map(TokenType::getPattern)
 				.collect(Collectors.joining("|"));
 	}
