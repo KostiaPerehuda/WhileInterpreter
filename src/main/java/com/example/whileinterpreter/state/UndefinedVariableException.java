de09834aa6bad21ff227caf259b73d431b13b06b
Kostiantyn Perehuda <kostiantyn.perehuda@student.manchester.ac.uk>
2022-03-10 16:06:04 +0000

    Configured project for IntellijIDEA

--- src/main/java/com/example/whileinterpreter/state/UndefinedVariableException.java
+++ src/main/java/com/example/whileinterpreter/state/UndefinedVariableException.java
@@ -1,23 +1,26 @@
 package com.example.whileinterpreter.state;
 
+import java.io.Serial;
+
 public class UndefinedVariableException extends RuntimeException {
-	
+
+	@Serial
 	private static final long serialVersionUID = 1L;
 
 	public UndefinedVariableException() {
 		super();
 	}
-	
+
 	public UndefinedVariableException(String message) {
 		super(message);
 	}
-	
+
 	public UndefinedVariableException(String message, Throwable cause) {
 		super(message, cause);
 	}
-	
+
 	public UndefinedVariableException(Throwable cause) {
 		super(cause);
 	}
-	
+
 }
