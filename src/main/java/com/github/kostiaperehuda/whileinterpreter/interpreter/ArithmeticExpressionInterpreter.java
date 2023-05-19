dcca6867d9a0e57fa5b0e88399a15aaadb5d359a
Kostiantyn Perehuda <kostiaperehuda@gmail.com>
2023-05-19 10:04:04 +0100

    Implement ArithmeticExpressionInterpreter

--- src/main/java/com/github/kostiaperehuda/whileinterpreter/interpreter/ArithmeticExpressionInterpreter.java
+++ src/main/java/com/github/kostiaperehuda/whileinterpreter/interpreter/ArithmeticExpressionInterpreter.java
@@ -1,12 +1,24 @@
 package com.github.kostiaperehuda.whileinterpreter.interpreter;
 
-import com.github.kostiaperehuda.whileinterpreter.ast.aexp.ArithmeticExpression;
+import com.github.kostiaperehuda.whileinterpreter.ast.aexp.*;
 import com.github.kostiaperehuda.whileinterpreter.state.State;
 
 public class ArithmeticExpressionInterpreter {
 
     public long evaluate(ArithmeticExpression expression, State state) {
-        return 0;
+        if (expression instanceof Const constant) {
+            return constant.number();
+        } else if (expression instanceof Variable variable) {
+            return state.get(variable.name());
+        } else if (expression instanceof Plus plus) {
+            return evaluate(plus.left(), state) + evaluate(plus.right(), state);
+        } else if (expression instanceof Minus minus) {
+            return evaluate(minus.left(), state) - evaluate(minus.right(), state);
+        } else if (expression instanceof Times times) {
+            return evaluate(times.left(), state) * evaluate(times.right(), state);
+        } else {
+            throw new IllegalArgumentException(expression.toString());
+        }
     }
 
 }
