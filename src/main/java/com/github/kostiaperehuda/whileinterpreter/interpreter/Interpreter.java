adc31e56da1184d60a341883bb6311b09e97536a
Kostiantyn Perehuda <kostiaperehuda@gmail.com>
2023-08-07 15:25:28 +0100

    Implement Assign Instruction

--- src/main/java/com/github/kostiaperehuda/whileinterpreter/interpreter/Interpreter.java
+++ src/main/java/com/github/kostiaperehuda/whileinterpreter/interpreter/Interpreter.java
@@ -1,11 +1,30 @@
 package com.github.kostiaperehuda.whileinterpreter.interpreter;
 
+import com.github.kostiaperehuda.whileinterpreter.ast.aexp.ArithmeticExpression;
+import com.github.kostiaperehuda.whileinterpreter.ast.aexp.Const;
+import com.github.kostiaperehuda.whileinterpreter.ast.cmd.Assign;
 import com.github.kostiaperehuda.whileinterpreter.ast.cmd.Command;
+import com.github.kostiaperehuda.whileinterpreter.ast.cmd.Skip;
 import com.github.kostiaperehuda.whileinterpreter.state.State;
+
+import java.math.BigInteger;
 
 public class Interpreter {
 
-    public void execute(Command skip, State state) {}
+    public void execute(Command command, State state) {
+        if (command instanceof Skip) return;
+
+        if (command instanceof Assign assign) {
+            state.put(assign.variableName(), evaluate(assign.expression()));
+        }
+    }
+
+    private BigInteger evaluate(ArithmeticExpression expression) {
+        if (expression instanceof Const constant) {
+            return constant.number();
+        }
+        return null;
+    }
 
     public static void run(String[] args) {
         /* TODO: implement */
--- src/test/java/com/github/kostiaperehuda/whileinterpreter/interpreter/InterpreterTest.java
+++ src/test/java/com/github/kostiaperehuda/whileinterpreter/interpreter/InterpreterTest.java
@@ -1,11 +1,14 @@
 package com.github.kostiaperehuda.whileinterpreter.interpreter;
 
+import com.github.kostiaperehuda.whileinterpreter.ast.aexp.Const;
+import com.github.kostiaperehuda.whileinterpreter.ast.cmd.Assign;
 import com.github.kostiaperehuda.whileinterpreter.ast.cmd.Skip;
 import com.github.kostiaperehuda.whileinterpreter.state.State;
 import org.junit.jupiter.api.Test;
 
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verifyNoInteractions;
+import java.math.BigInteger;
+
+import static org.mockito.Mockito.*;
 
 class InterpreterTest {
 
@@ -17,6 +20,16 @@ class InterpreterTest {
         new Interpreter().execute(command, state);
 
         verifyNoInteractions(state);
+    }
+
+    @Test
+    void shouldEvaluateConstantExpressionAndPutItsResultIntoTheProgramStateWhenExecutingAssignInstruction() {
+        var state = mock(State.class);
+        var command = new Assign("dummy", new Const(BigInteger.TEN));
+
+        new Interpreter().execute(command, state);
+
+        verify(state).put("dummy", BigInteger.TEN);
     }
 
 }
